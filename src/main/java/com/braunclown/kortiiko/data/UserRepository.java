f54fcd428ccb38b6a441eeadb968dffddc8845b9
braunclown <ryzhcat@gmail.com>
2024-02-16 11:34:26 +0300

    Fixed the incorrect login bug

--- src/main/java/com/braunclown/kortiiko/data/UserRepository.java
+++ src/main/java/com/braunclown/kortiiko/data/UserRepository.java
@@ -3,8 +3,10 @@ package com.braunclown.kortiiko.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.lang.Nullable;
 
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
 
+    @Nullable
     User findByUsername(String username);
 }
